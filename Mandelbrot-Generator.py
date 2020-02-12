c045ed205cdc764816af01c7a7a07ebf68d9bf65
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-12 16:37:01 +0100

    Update Mandelbrot-Generator.py
    updated greyscale versions

--- Mandelbrot-Generator.py
+++ Mandelbrot-Generator.py
@@ -18,10 +18,9 @@ if Q == "y":
     glow = int(input("White glow (positive integer between 0-20 f.ex): "))
 else:
     Q2 = (input("Options:\n-White outlines on black background (wout) \n-Black outlines on white background (bout) \n-Black on white background (b)\n-White on black background (w)\n")).lower()
-    if Q2== "wout":
-        glow = int(input("White glow (positive integer between 0-20 f.ex): "))
-        if glow == 0:
-            glow = 1
+    glow = int(input("glow (positive integer between 1-10 f.ex): "))
+    if glow == 0:
+        glow = 1
 
 W = int(input("Width of resolution (pixels): "))
 H = int(input("Height of resolution (pixels): "))
@@ -33,27 +32,29 @@ yDist = xDist * ratio
 yvalue = float(input("Y value (= will be the middle of the screen): "))
 img = Image.new('HSV', (W, H))
 draw = ImageDraw.Draw(img)
+
 for x in range(0, W):
     for y in range(0, H):
         c = complex(xStart + abs(xDist) * (x / W), (yvalue + (yDist/2)) - yDist * (y / H))
         cIt = mandelbrot(c,maxIt)
+
         color = int((255 * cIt) / maxIt)
         if Q == "n":
             if Q2 == "wout":
                 draw.point([x, y], (0, 0, 0 if cIt == maxIt else glow*color))
             elif Q2 == "bout":
-                draw.point([x, y], (0, 0, 235 if cIt == maxIt else 255-2*cIt))
+                draw.point([x, y], (0, 0, 255 if cIt == maxIt else 255-glow*color))
             elif Q2 == "b":
-                draw.point([x, y], (0, 0, 0 if cIt == maxIt else 255))
+                draw.point([x, y], (0, 0, 0 if cIt == maxIt else 255 - glow*color))
             elif Q2 == "w":
-                draw.point([x, y], (0, 0, 255 if cIt == maxIt else 0))
+                draw.point([x, y], (0, 0, 255 if cIt == maxIt else glow * color))
         else:
 
             if color + offset > 255:
                 Color = (color+offset)-255
             else:
                 Color = color+offset
-            saturation = 255 - glow * cIt
+            saturation = 255 - glow * color
             draw.point([x, y], (Color, saturation, 0 if cIt == maxIt else light))
 
 img.convert('RGB').save("c:\\Users\\{}\\Desktop\\image.png".format(host))
